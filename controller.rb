9847f99ea95ad21d1cfe083948975d732f5f7857
Alberto Vena <kennyadsl@gmail.com>
2012-01-16 13:02:01 +0100

    Adds some dynamic variables to layout

--- controller.rb
+++ controller.rb
@@ -1,2 +1,10 @@
 ignore "readme.md"
 layout /.*\.html.haml/ => 'layout.html.haml'
+
+# Set default variables useful in every page
+before /.*html\./ do
+    @lang = "en"
+    @author = "Your Name"
+    @title = "Website Title" 
+    @description = "Website description"
+end
