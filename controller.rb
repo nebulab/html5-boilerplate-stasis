eeb1e5bda78841049971cf1c07312c3aed513eee
Alberto Vena <kennyadsl@gmail.com>
2012-01-17 17:11:25 +0100

    adds analytics tracker

--- controller.rb
+++ controller.rb
@@ -1,10 +1,13 @@
 ignore "readme.md"
 layout /.*\.html.haml/ => 'layout.html.haml'
 
-# Set default variables useful in every page
 before /.*html\./ do
+    # Set default variables useful in every page
     @lang = "en"
     @author = "Your Name"
     @title = "Website Title" 
     @description = "Website description"
+    
+    # Set Google Analytics tracker
+    @tracker = "UAXXXXXXXX1"
 end
